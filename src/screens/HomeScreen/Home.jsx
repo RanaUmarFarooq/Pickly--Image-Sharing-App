b005f21c3404e506b4a357ab0cc3bb244d5f0462
Umar Farooq <92082617+RanaUmarFarooq@users.noreply.github.com>
2024-05-20 00:04:40 +0500

    Header Compeletion

--- src/screens/HomeScreen/Home.jsx
+++ src/screens/HomeScreen/Home.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Home.module.css";
 import SideBar from "../../components/SideBar/SideBar";
+import { FiSearch } from "react-icons/fi";
+import { BsStars } from "react-icons/bs";
 
 const Home = () => {
+  const [isTyping, setIsTyping] = useState(false);
+  const handleChange = (event) => {
+    if (event.target.value) {
+      setIsTyping(true);
+    } else {
+      setIsTyping(false);
+    }
+  };
   return (
     <div className={styles.container}>
-      <div className={styles.sideBar}>
-        <SideBar />
-      </div>
+      <SideBar />
       <div className={styles.main_content}>
-        <h1>Photos</h1>
+        <div className={styles.search_container}>
+          <div
+            className={`${styles.text_input} ${
+              isTyping ? styles.focsued_input : ""
+            }`}
+          >
+            <input
+              type="text"
+              placeholder="Search over 10,000 Images"
+              className={styles.input}
+            />
+            <button className={styles.btn_search}>
+              <FiSearch className={styles.icon}></FiSearch>
+            </button>
+          </div>
+          <button className={styles.btn_upload}>
+            <span className={styles.upload_icon}>
+              <BsStars />
+            </span>
+            Upload Image
+          </button>
+        </div>
+        <div>Images</div>
       </div>
     </div>
   );
