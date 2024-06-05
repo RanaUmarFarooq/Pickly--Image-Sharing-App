ebbdaff989dd733cc3d69d88f6ee40083ead5e86
Umar Farooq <92082617+RanaUmarFarooq@users.noreply.github.com>
2024-06-05 21:31:14 +0500

    image modal

--- src/components/Image Modal/ImageModal.jsx
+++ src/components/Image Modal/ImageModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./ImageModal.module.css";
+import { LuImagePlus } from "react-icons/lu";
 const ImageModal = ({ show, handleClose, handleSubmit }) => {
   return (
     show && (
@@ -7,13 +8,33 @@ const ImageModal = ({ show, handleClose, handleSubmit }) => {
         <div className={styles.modalContent}>
           <form action="">
             <div className={styles.image_Input_container}>
-              <h3>Image</h3>
+              <LuImagePlus className={styles.image_upload_icon} />
+              <h4>Select an Image</h4>
             </div>
             <div className={styles.image_details_container}>
-              <h3>input</h3>
+              <div className={styles.input_element}>
+                <label htmlFor="title">Title</label>
+                <input type="text" />
+              </div>
+              <div className={styles.input_element}>
+                <label htmlFor="title">Select Catagory</label>
+                <select name="" id="">
+                  <option value="animal">Animal</option>
+                  <option value="animal">Art</option>
+                  <option value="animal">Characters</option>
+                  <option value="animal">Food & drinks</option>
+                  <option value="animal">Animal</option>
+                </select>
+              </div>
+              <div className={styles.input_element}>
+                <label htmlFor="title">Description</label>
+                <textarea name="" id=""></textarea>
+              </div>
             </div>
           </form>
-          <button onClick={handleClose}>close</button>
+          <button className={styles.close_btn} onClick={handleClose}>
+            close
+          </button>
         </div>
       </div>
     )
